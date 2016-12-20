8ea302bdc01a0d21fea8d366b4e5db5e571f01d9
Lindsey Jacks <linzjax@gmail.com>
2016-12-20 14:17:58 -0500

    datatables cleanup

--- cadasta/core/static/js/dataTables.selectFiltering.js
+++ cadasta/core/static/js/dataTables.selectFiltering.js
@@ -1,37 +1,38 @@
-(function(window, document, $) {
-    $(document).on('init.dt', function(e, dtSettings) {
-        if ( e.namespace !== 'dt' ) {
+function setup_filter(document, $) {
+    'use strict';
+
+    $(document).on('init.dt', function (e, dtSettings) {
+        if (e.namespace !== 'dt') {
             return;
         }
-        addSelectOptions = function(){
-            aData = ['Active', 'Archived', 'All']
-            var r='<label><select class="form-control input-sm" id="archive-filter">', i, iLen=aData.length;
-            for ( i=0 ; i<iLen ; i++ )
-            {
-                r += '<option value="'+aData[i]+'">'+aData[i]+'</option>';
+        var addSelectOptions = function () {
+            var aData = ['Active', 'Archived', 'All'];
+            var r = '<label><select class="form-control input-sm" id="archive-filter">', i, iLen = aData.length;
+            for (i = 0; i < iLen; i += 1) {
+                r += '<option value="' + aData[i] + '">' + aData[i] + '</option>';
             }
-            return r+'</select></label>';
-        }
+            return r + '</select></label>';
+        };
 
         var table = $('#DataTables_Table_0').DataTable();
-        if ($(".unarchived").length ){
-            table.order([1, 'asc']).draw()
+        if ($(".unarchived").length) {
+            table.order([1, 'asc']).draw();
         }
 
-        if ($(".archived").length ){
-            dtSettings.nTableWrapper.childNodes[0].childNodes[0].innerHTML += addSelectOptions()
+        if ($(".archived").length) {
+            dtSettings.nTableWrapper.childNodes[0].childNodes[0].innerHTML += addSelectOptions();
             table.columns(0).search('False').draw();
 
-            $('input').on( 'keyup', function () {
-                table.search( this.value ).draw();
+            $('input').on('keyup', function () {
+                table.search(this.value).draw();
             });
 
             $('#archive-filter').change(function () {
-                var value = ''
+                var value = '';
                 table.search(value).draw();
-                var selection = $('#archive-filter').val()
+                var selection = $('#archive-filter').val();
                 if (selection === 'Active') {
-                    value = 'False'
+                    value = 'False';
                 } else if (selection === 'Archived') {
                     value = 'True';
                 }
@@ -39,53 +40,52 @@
             });
         }
     });
-})(window, document, jQuery);
+}
 
-(function(window, document, $) {
-    $(document).on('init.dt', function(e, dtSettings) {
-        if ( e.namespace !== 'dt' ) {
+setup_filter(document, jQuery);
+
+
+function setup_search_filter(document, $) {
+    'use strict';
+
+    $(document).on('init.dt', function (e, dtSettings) {
+        if (e.namespace !== 'dt') {
             return;
         }
-        // addSelectOptions = function(){
-        //     aData = ['Show all', 'Show only locations', 'Show only relationships', 'Show only parties', 'Show only resources']
-        //     var r='<label><select class="form-control input-sm" id="entity-type-filter">', i, iLen=aData.length;
-        //     for ( i=0 ; i<iLen ; i++ )
-        //     {
-        //         r += '<option value="'+aData[i]+'">'+aData[i]+'</option>';
-        //     }
-        //     return r+'</select></label>';
-        // }
+        var addSelectOptions = function () {
+            var aData = ['Show all', 'Show only locations', 'Show only relationships', 'Show only parties', 'Show only resources'];
+            var r = '<label><select class="form-control input-sm" id="entity-type-filter">', i, iLen = aData.length;
+            for (i = 0; i < iLen; i += 1) {
+                r += '<option value="' + aData[i] + '">' + aData[i] + '</option>';
+            }
+            return r + '</select></label>';
+        };
 
         var table = $('#search-results').DataTable();
-        if ($(".entity-type").length ){
-            table.order([1, 'asc']).draw()
+        if ($(".entity-type").length) {
+            table.order([1, 'asc']).draw();
         }
 
-        // if ($(".entity-type").length ){
-        //     dtSettings.nTableWrapper.childNodes[0].childNodes[0].innerHTML += addSelectOptions()
-        //     // table.columns(0).search('False').draw();
-        // }
+        if ($(".entity-type").length) {
+            dtSettings.nTableWrapper.childNodes[0].childNodes[0].innerHTML += addSelectOptions();
+        }
 
-        // $('input').on( 'keyup', function () {
-        //     table.search( this.value ).draw();
-        // });
-
-        // table.order.listener( '#sorter', 1 );
-
-        // $('#entity-type-filter').change(function () {
-        //     var value = ''
-        //     table.search(value).draw();
-        //     var selection = $('#entity-type-filter').val()
-        //     if (selection === 'Show only locations') {
-        //         value = 'Location'
-        //     } else if (selection === 'Show only relationships') {
-        //         value = 'Relationship';
-        //     } else if (selection === 'Show only parties') {
-        //         value = 'Party';
-        //     } else if (selection === 'Show only resources') {
-        //         value = 'Resource';
-        //     }
-        //     table.columns(0).search(value).draw(false);
-        // });
+        $('#entity-type-filter').change(function () {
+            var value = '';
+            table.search(value).draw();
+            var selection = $('#entity-type-filter').val();
+            if (selection === 'Show only locations') {
+                value = 'Location';
+            } else if (selection === 'Show only relationships') {
+                value = 'Relationship';
+            } else if (selection === 'Show only parties') {
+                value = 'Party';
+            } else if (selection === 'Show only resources') {
+                value = 'Resource';
+            }
+            table.columns(0).search(value).draw();
+        });
     });
-})(window, document, jQuery);
+}
+
+setup_search_filter(document, jQuery);
--- cadasta/search/views/api.py
+++ cadasta/search/views/api.py
@@ -5,73 +5,12 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from tutelary.mixins import APIPermissionRequiredMixin
+from organization.models import Project
+from spatial.models import SpatialUnit
+from party.models import Party, TenureRelationship
+from resources.models import Resource
 
 from .mixins import SearchResultsMixin
-
-
-# ~~~~~~~~~~~~~~~~
-# Just for testing: DELETE later
-# ~~~~~~~~~~~~~~~~
-test_results = {
-          "took": 63,
-          "timed_out": 'false',
-          "_shards": {
-            "total": 5,
-            "successful": 5,
-            "failed": 0
-          },
-          "hits": {
-            "total": 24,
-            "max_score": 'null',
-            "hits": [{
-              "_index": "project-slug",
-              "_type": "location",
-              "_id": "zpeqs5uh39dhi8gr6ec3t3w5",
-              "sort": [0],
-              "_score": 'null',
-              "_source": {
-                "type": "CB",
-                "name": "Long Island",
-                "notes": "Nothing to see here.",
-                "acquired_when": "2016-12-16",
-                "quality": "text",
-                "acquired_how": "LH"}
-            }, {
-              "_index": "project-slug",
-              "_type": "party",
-              "_id": "xqm9r3nymxv8pck52zgvz7t5",
-              "sort": [1],
-              "_score": 'null',
-              "_source": {
-                "name": "Big Bird",
-                "type": "IN",
-                "notes": ("He\'s just a big bird. "
-                          "Not the guy from seasame street..")}
-            }, {
-              "_index": "project-slug",
-              "_type": "tenure_rel",
-              "_id": "3xcg8erv2yi22scm3kve8maa",
-              "sort": [1],
-              "_score": 'null',
-              "_source": {
-                "party": "xqm9r3nymxv8pck52zgvz7t5",
-                "spatial_unit": "zpeqs5uh39dhi8gr6ec3t3w5",
-                "tenure_type": "CU",
-                "notes": "But he does have customary rights."}
-            }, {
-              "_index": "project-slug",
-              "_type": "resource",
-              "_id": "u25ayvgg57zqjrjkkshryyw5",
-              "sort": [1],
-              "_score": 'null',
-              "_source": {
-                "name": "Goat",
-                "description": "Let's pretend there's a description.",
-                "original_file": "baby_goat.jpeg"}
-            },
-            ]
-          }
-        }
 
 
 class Search(APIPermissionRequiredMixin,
@@ -100,70 +39,142 @@ class Search(APIPermissionRequiredMixin,
         return r
 
     def get(self, request, *args, **kwargs):
+        print('ES get called.')
         query = request.query_params.get('q')
         results = []
 
-        if query:
-            r = self.query_es(query, kwargs['project'])
-            print(r.json())
-            initial_results = self.format_search_results(r.json())
-            print(initial_results)
+        # if query:
+        #     r = self.query_es(query, kwargs['project'])
+        #     initial_results = self.format_search_results(r.json())
 
-        #     # Parse and translate search results
-        #     for result in r.json()['hits']['hits']:
-        #         rec_type = result['_type']
-        #         id = result['_id']
-        #         model = {
-        #             'location': SpatialUnit,
-        #             'party': Party,
-        #             'tenure_rel': TenureRelationship,
-        #             'resource': Resource,
-        #         }[rec_type]
-        #         record = model.objects.get(id=id)
-        #         # TODO: Perform Tutelary permissions checking
-        #         results.append([
-        #             record.ui_class_name,
-        #             '<a href="{}">{}</a>'.format(
-        #                 record.ui_detail_url,
-        #                 record.name,
-        #             ),
-        #         ])
+        # ~~~~~~~~~~~~~~~~~~~
+        # JUST FOR TESTING
+        # ~~~~~~~~~~~~~~~~~~~~
+        su = SpatialUnit.objects.filter(project__slug=kwargs['project'])
+        party = Party.objects.filter(project__slug=kwargs['project'])
+        tenure_rel = TenureRelationship.objects.filter(project__slug=kwargs['project'])
+        resource = Resource.objects.filter(project__slug=kwargs['project'])
 
-            for record in initial_results:
-                html = (
-                 "<td>"
-                 "<table>"
-                 "<tr>"
-                 "<div>{entity_type}</div>"
-                 "<h4><a href='{url}'>{main_label}</a></h4>"
-                 "</tr>").format(
-                    entity_type=record['entity_type'],
-                    main_label=record['main_label'],
-                    url=record['url'])
+        initial_results = get_test_results(su, party, tenure_rel, resource)
+        initial_results = self.format_search_results(initial_results)
 
-                if record.get('image', None):
-                    html += (
-                        '<tr>'
-                        '<img src="{image}" class="thumb-60">'
-                        '</tr>').format(
-                            image=record['image'])
+        for record in initial_results:
+            html = (
+             "<td>"
+             "<table>"
+             "<tr>"
+             "<div>{entity_type}</div>"
+             "<h4><a href='{url}'>{main_label}</a></h4>"
+             "</tr>").format(
+                entity_type=record['entity_type'],
+                main_label=record['main_label'],
+                url=record['url'])
 
-                for key, attr in record['attributes']:
-                    html += (
-                      '<tr>'
-                      '<td>{key}</td>'
-                      '<td style="padding-left:50px;">{attr}</td>'
-                      '</tr>').format(
-                        key=key,
-                        attr=attr)
-
+            if record.get('image', None):
                 html += (
-                  '</table>'
-                  '</td>'
-                )
-                results.append([
-                    record['entity_type'],
-                    record['main_label'],
-                    html])
+                    '<tr>'
+                    '<img src="{image}" class="thumb-60">'
+                    '</tr>').format(
+                        image=record['image'])
+
+            for key, attr in record['attributes']:
+                html += (
+                  '<tr>'
+                  '<td>{key}</td>'
+                  '<td style="padding-left:50px;">{attr}</td>'
+                  '</tr>').format(
+                    key=key,
+                    attr=attr)
+
+            html += (
+              '</table>'
+              '</td>'
+            )
+            results.append([
+                record['entity_type'],
+                record['main_label'],
+                html])
 
         return Response({'results': results})
+
+
+# ~~~~~~~~~~~~~~~~~~~~~
+# DELETE LATER
+# ~~~~~~~~~~~~~~~~~~~~~
+
+def get_test_results(spatial_units=[], parties=[],
+                     tenure_rels=[], resources=[]):
+    result = {
+          "took": 63,
+          "timed_out": 'false',
+          "_shards": {
+            "total": 4,
+            "successful": 4,
+            "failed": 0
+          },
+          "hits": {
+            "total": 4,
+            "max_score": 'null',
+            "hits": []
+          }
+        }
+
+    for su in spatial_units:
+        result['hits']['hits'].append({
+                "_index": su.project.id,
+                "_type": "location",
+                "_id": su.id,
+                "sort": [0],
+                "_score": 'null',
+                "_source": {
+                  "type": "CB",
+                  "name": "Long Island",
+                  "notes": "Nothing to see here.",
+                  "acquired_when": "2016-12-16",
+                  "quality": "text",
+                  "acquired_how": "LH"}
+              })
+    for party in parties:
+        result['hits']['hits'].append({
+                "_index": party.project.slug,
+                "_type": "party",
+                "_id": party.id,
+                "sort": [1],
+                "_score": 'null',
+                "_source": {
+                  "name": "Big Bird",
+                  "type": "IN",
+                  "gender": "M",
+                  "homeowner": "yes",
+                  "dob": "1951-05-05"
+                }
+              })
+    for tenure_rel in tenure_rels:
+        result['hits']['hits'].append({
+              "_index": tenure_rel.project.slug,
+              "_type": "tenure_rel",
+              "_id": tenure_rel.id,
+              "sort": [1],
+              "_score": 'null',
+              "_source": {
+                "party": "xqm9r3nymxv8pck52zgvz7t5",
+                "spatial_unit": "zpeqs5uh39dhi8gr6ec3t3w5",
+                "tenure_type": "CU",
+                "notes": "PBS is the best."}
+            })
+    for resource in resources:
+        result['hits']['hits'].append({
+              "_index": resource.project.slug,
+              "_type": "resource",
+              "_id": resource.id,
+              "sort": [1],
+              "_score": 'null',
+              "_source": {
+                "name": resource.name,
+                "description": resource.description,
+                "original_file": resource.original_file}
+            },)
+
+    result['hits']['total'] = len(result['hits']['hits'])
+
+    return result
